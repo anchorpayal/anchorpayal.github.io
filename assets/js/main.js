cf2e4cd8a5b5a3db147f1502b167e5615b7f15a0
Payal <anchorpayal@gmail.com>
2018-02-27 02:05:56 +0530

    Lazy load youtube frame

--- assets/js/main.js
+++ assets/js/main.js
@@ -275,6 +275,13 @@ function init() {
             imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
         }
     }
+
+    var imgDefer = document.getElementsByClassName('video');
+    for (var i=0; i<imgDefer.length; i++) {
+        if(imgDefer[i].getAttribute('data-src')) {
+            imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+        }
+    }
 }
 
 window.onload = init;
