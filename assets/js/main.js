b50fb993dd88746b573fc60fc1a7e6e430a9d703
Payal <anchorpayal@gmail.com>
2018-02-27 23:54:51 +0530

    Add gallery and featured event section

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,7 +101,6 @@
 	};
 
 	$(function() {
-
 		var	$window = $(window),
 			$body = $('body'),
 			$wrapper = $('#wrapper'),
@@ -265,6 +264,109 @@
 
 	});
 
+    $('#gallery a').magnificPopup({
+        items: [
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/1.jpg",
+                title: "Regional meet for Dr Reddys"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/2.jpg",
+                title: "Destination wedding event in Belgaum"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/3.jpg",
+                title: "Launch of Clai Brand in Pune in presence of Umesh Kamat, Santosh Juvekar and Swapnil Joshi"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/4.jpg",
+                title: "Building Estate Launch of Tirupati Homes in Aurangabad"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/5.jpg",
+                title: "Rewards Night for Veiva"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/6.jpg",
+                title: "Dandiya event in pune in presence of Zareen Khan"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/7.jpg",
+                title: "Rewards Night for Veiva"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/8.jpg",
+                title: "Launch of Clai Brand in Pune in presence of Umesh Kamat, Santosh Juvekar and Swapnil Joshi"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/9.jpg",
+                title: "Dandiya event in pune in presence of Zareen Khan"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/10.jpg",
+                title: "Launch of Clai Brand in Pune in presence of Umesh Kamat, Santosh Juvekar and Swapnil Joshi"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/11.jpg",
+                title: "Wedding event in Qatar for the Abbas Family in presence of Pakistani player"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/12.jpg",
+                title: "Rewards and recognition Night for Syntel in Mumbai"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/13.jpg",
+                title: "Rewards and recognition Night for Syntel in Mumbai"
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/14.jpg",
+                title: "Annual meet In Mumbai for Dr Reddys"
+            }
+        ],
+        gallery: {
+            enabled: true
+        },
+        type: 'image' // this is default type
+    });
+
+    $('#udaipur').magnificPopup({
+        items: [
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/udaipur/01.jpg",
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/udaipur/02.jpg",
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/udaipur/03.jpg",
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/udaipur/04.jpg",
+            }
+        ],
+        gallery: {
+            enabled: true
+        },
+        type: 'image' // this is default type
+    });
+
+    $('#dy_patil').magnificPopup({
+        items: [
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/dy_patil/01.jpg",
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/dy_patil/02.jpg",
+            },
+            {
+                src: "https://payalb.s3.amazonaws.com/gallery/dy_patil/03.jpg",
+            }
+        ],
+        gallery: {
+            enabled: true
+        },
+        type: 'image' // this is default type
+    });
 })(jQuery);
 
 
